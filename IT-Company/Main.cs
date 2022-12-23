3d02c511e9eb0427eafa918c4477aa6e1418d5ee
kennet <vovashevalev@gmail.com>
2022-12-23 18:45:35 +0100

    Добавьте файлы проекта.

---
+++ IT-Company/Main.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+
+namespace IT_Company
+{
+    class Main
+    {
+        public static void main(String[] args)
+        {
+
+
+
+        }
+
+    }
+
+    class Worker {
+        public Worker(double salary, String typeOfProjects, int[] term) { 
+        
+        
+        }
+    }
+
+    class SoftwareEngineer : Worker {  // добавить скиллы к каждому классуб например знание япов и 
+    
+    }
+
+    class Tester : Worker { 
+    
+    }
+
+    class DevOpsEngineer : Worker { 
+    
+    }
+}
