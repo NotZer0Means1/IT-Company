46336cd7bb0ad0942bd77d8bca5102735b549ca2
kennet <vovashevalev@gmail.com>
2023-01-23 21:54:45 +0100

    Revert "Revert "Revert "added some functions"""
    This reverts commit b52db02e8cfe29179dbe7873ea5ce43e808ba5ee.

--- IT-Company/Main.cs
+++ IT-Company/Main.cs
@@ -1,12 +1,9 @@
 ﻿using System;
-using System.IO;
 
 namespace IT_Company
 {
     class Company
     {
-        public const String PathToWorkers = "D:\\programming\\visualstudio\\IT-Company\\IT-Company\\WorkerExamples.txt";
-        public const String PathToClients = "D:\\programming\\visualstudio\\IT-Company\\IT-Company\\ClientExamples.txt";
 
         struct Employee {
 
@@ -60,7 +57,6 @@ namespace IT_Company
             Console.ForegroundColor = ConsoleColor.Green;
             Console.WriteLine("7. list of available statistics.");
             Console.ForegroundColor = ConsoleColor.Blue;
-            Console.SetCursorPosition(0, 4);
 
         }
 
@@ -78,45 +74,17 @@ namespace IT_Company
 
         static void chooseMenuItem() {
 
-            int k = 1;
-            int position = 4;
-            while (true) {
-                ConsoleKeyInfo key = Console.ReadKey(true);
-                
-                if (key.Key == ConsoleKey.DownArrow) {
+            repeat:
+            Console.WriteLine("Choose menu item: ");
+            int k = Convert.ToInt32(Console.ReadLine());
 
-                    if (position + 1 == 11)
-                    {
-                        position = 4;
-                    }
-                    else
-                    {
-                        k += 1;
-                        position += 1;
-                    }
+            if (k > 7) {
 
-                }
-                else if (key.Key == ConsoleKey.UpArrow) {
-
-                    if (position - 1 == 3)
-                    {
-                        position = 10;
-                    }
-                    else
-                    {
-                        k -= 1;
-                        position -= 1;
-                    }
-
-                }
-                else if (key.Key == ConsoleKey.Enter) { 
-
-                    break; 
-
-                }
-                Console.SetCursorPosition(0, position);
-
+                Console.WriteLine("Choose the correct menu item!");
+                goto repeat; 
+            
             }
+
             Console.Clear();
 
             switch (k)
@@ -126,7 +94,7 @@ namespace IT_Company
                     createNewProfileOfEmployee();
                     break;
                 case 2:
-                    createNewProfileOfClient();
+                    createNewprofileOfClient();
                     break;
                 case 3:
                     changeProfileOfEmployee();
@@ -141,34 +109,18 @@ namespace IT_Company
                     deleteProfileOfClient();
                     break;
                 case 7:
+
                     break;
 
             }
         }
 
-        static void ReadFile(String path) {
-
-             using (StreamReader reader = new StreamReader(path)) {
-
-                string text =  reader.ReadToEnd();
-                Console.WriteLine(text);
-
-            }
-            Console.ReadKey();
-            Console.Clear();
-        
-        }
-
-
         static void successNotification() {
 
-            Console.Clear();
             ConsoleColor oldForegroundColor = Console.ForegroundColor;
             Console.ForegroundColor = ConsoleColor.Green;
             Console.WriteLine("New profile was created succesfully!");
             Console.ForegroundColor = oldForegroundColor;
-            Console.ReadKey();
-            Console.Clear();
 
         }
 
@@ -189,26 +141,14 @@ namespace IT_Company
             employee.salary = Convert.ToDouble(Console.ReadLine());
             Console.WriteLine("Input Employee's term of work (in months): ");
             employee.termOfWork = Convert.ToInt32(Console.ReadLine());
-
-            using (StreamWriter streamWriter = new StreamWriter(PathToWorkers, true))
-            {
-
-                streamWriter.WriteLine("\n");
-                streamWriter.WriteLine(employee.fullname);
-                streamWriter.WriteLine(employee.age);
-                streamWriter.WriteLine(employee.specialization);
-                streamWriter.WriteLine(employee.position);
-                streamWriter.WriteLine(employee.project);
-                streamWriter.WriteLine(employee.salary);
-                streamWriter.WriteLine(employee.termOfWork);
-
-            }
-
+            Console.Clear();
             successNotification();
+            Console.ReadKey();
+            Console.Clear();
 
         }
 
-        static void createNewProfileOfClient() {
+        static void createNewprofileOfClient() {
 
             Client client = new Client();
             Console.WriteLine("Input Client's fullname: ");
@@ -219,39 +159,17 @@ namespace IT_Company
             client.task = Console.ReadLine();
             Console.WriteLine("Input Client's capital: ");
             client.capital = Convert.ToDouble(Console.ReadLine());
-
-            using (StreamWriter streamWriter = new StreamWriter(PathToClients, true))
-            {
-
-                streamWriter.WriteLine("\n");
-                streamWriter.WriteLine(client.fullname);
-                streamWriter.WriteLine(client.age);
-                streamWriter.WriteLine(client.task);
-                streamWriter.WriteLine(client.capital);
-
-            }
-
+            Console.Clear();
             successNotification();
+            Console.ReadKey();
+            Console.Clear();
 
         }
 
-        static void changeProfileOfEmployee() {
-            Console.Clear();
-            Console.WriteLine("Choose which employee you want to change: ");
-            Console.WriteLine("Choose what attribute(s) you want to change: ");
-            String fullname = "1. fullname";
-            String age = "2. age";
-            String specialization = "3. specialization";
-            String position = "4. position";
-            String project = "5. project";
-            String salary = "6. salary";
-            String term = "7. term of work";
+        static void changeProfileOfEmployee() { 
+        
             
-            
-
-
-
-            
+        
         }
 
         static void changeProfileOfClient() {
